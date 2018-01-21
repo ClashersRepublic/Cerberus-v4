823f5f09ebbc65843742f2223f77e8de25238951
Mike Chiappe <mike.chiappe@gmail.com>
2018-01-22 00:41:36 +0100

    Debugger.cs added

---
+++ ClashersRepublic/ClashersRepublic.Magic/ClashersRepublic.Magic.Titan/Debug/Debugger.cs
@@ -0,0 +1,64 @@
+﻿namespace ClashersRepublic.Magic.Titan.Debug
+{
+    using System.IO;
+    using System.Runtime.CompilerServices;
+
+    public static class Debugger
+    {
+        public delegate void DebugEventHandler(object sender, DebugEventArgs args);
+
+        public static event DebugEventHandler LogEvent;
+        public static event DebugEventHandler WarningEvent;
+        public static event DebugEventHandler ErrorEvent;
+
+        /// <summary>
+        ///     Do assert the specified message.
+        /// </summary>
+        public static bool DoAssert(bool assertion, string assertionError, [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            if (!assertion)
+            {
+                Debugger.ErrorEvent(Path.GetFileName(callerFilePath) + ":" + callerLineNumber, new DebugEventArgs(assertionError));
+            }
+
+            return assertion;
+        }
+
+        /// <summary>
+        ///     Logs the specified message.
+        /// </summary>
+        public static void Log(string log, [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            Debugger.LogEvent(Path.GetFileName(callerFilePath) + ":" + callerLineNumber, new DebugEventArgs(log));
+        }
+
+        /// <summary>
+        ///     Logs the specified warning message.
+        /// </summary>
+        public static void Warning(string log, [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            Debugger.WarningEvent(Path.GetFileName(callerFilePath) + ":" + callerLineNumber, new DebugEventArgs(log));
+        }
+
+        /// <summary>
+        ///     Logs the specified error message.
+        /// </summary>
+        public static void Error(string log, [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            Debugger.ErrorEvent(Path.GetFileName(callerFilePath) + ":" + callerLineNumber, new DebugEventArgs(log));
+        }
+    }
+
+    public class DebugEventArgs
+    {
+        public string Text;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DebugEventArgs" /> class.
+        /// </summary>
+        public DebugEventArgs(string text)
+        {
+            this.Text = text;
+        }
+    }
+}
